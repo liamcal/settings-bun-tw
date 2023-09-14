4a8ec5c1cf70117027842c03ba4ee1ff863905ca
Liam Callaway <lcallaway@campaignmonitor.com>
2023-09-14 21:52:49 +1000

    Update header

--- src/app/components/HeaderRow.tsx
+++ src/app/components/HeaderRow.tsx
@@ -1,10 +1,21 @@
+import { CloudSnowIcon } from '../Icons/CloudSnowIcon';
 import { SearchInput } from './SearchInput';
 
 const HeaderRow = () => {
     return (
         <>
-            <div className="flex flex-row items-center justify-between">
-                <h1 className="text-2xl">Heading</h1>
+            <div className="flex flex-row items-center justify-between pt-4">
+                <div className="flex flex-row gap-4">
+                    <div className="item-center flex rounded-md border p-4">
+                        <div className="h-6 w-6">
+                            <CloudSnowIcon />
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h1 className="text-2xl">Trysnow</h1>
+                        <p className="text-sm text-gray-500">trysnow.com</p>
+                    </div>
+                </div>
                 <SearchInput />
             </div>
             <hr />
